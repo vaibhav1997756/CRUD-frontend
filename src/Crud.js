99cd54bebe826217145c5167297a28cad00d4252
Vaibhav Singh <vaibhavsinghthebest@gmail.com>
2024-10-09 17:21:30 +0530

    first commit

---
+++ src/Crud.js
@@ -0,0 +1,104 @@
+// import React,{useEffect,useState} from 'react'
+// import {Link} from 'react-router-dom';
+// import axios from 'axios'
+
+// const Crud = () => {
+   
+
+//     const [crud,setCrud]=useState([]);
+//     useEffect(()=>{
+//      axios.get('http://localhost:8080/')
+//         .then(res => setCrud(res.data))
+//         .catch(err =>console.log(err));
+  
+//     },[])
+//   return (
+//     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+//         <div className='w-50 bg-white rounded'>
+//             <Link to='/create' className='btn btn-success'>Add +</Link>
+//            <table className='table'>
+//                <thead>
+//                 <tr>
+//                      <th>Name</th>
+//                      <th>Email</th>
+//                      <th>Action</th>
+//                      </tr>
+//                </thead>
+//                <tbody>
+//                     {
+//                         crud.map((data,i)=>(
+//                             <tr key={i}>
+//                                 <td>{data.name}</td>
+//                                 <td>{data.email}</td>
+//                                 <td>
+//                                     <button className='btn btn-primary'>Update</button>
+//                                     <button className='btn btn-danger'>Delete</button>
+//                                 </td>
+//                             </tr>
+//                         ))
+//                     }
+//                </tbody>
+//            </table>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Crud
+
+
+
+import React, { useEffect, useState } from 'react';
+//import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Crud = () => {
+    const [usercrud, setCrud] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/') // Ensure the correct API endpoint
+            .then(res => setCrud(res.data))
+            .catch(err => console.log(err));
+    }, []);
+
+    const handleUpdate = (id) => {
+        // Add logic for updating the item
+        console.log(`Update item with id: ${id}`);
+    };
+
+    const handleDelete = (id) => {
+        // Add logic for deleting the item
+        console.log(`Delete item with id: ${id}`);
+    };
+
+    return (
+        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+            <div className="w-50 bg-white rounded p-3">
+                <button  className="btn btn-success mb-3">Add +</button>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {usercrud.map((data) => (
+                            <tr key={data.id}>
+                                <td>{data.name}</td>
+                                <td>{data.email}</td>
+                                <td>
+                                    <button className="btn btn-primary me-2" onClick={() => handleUpdate(data.id)}>Update</button>
+                                    <button className="btn btn-danger" onClick={() => handleDelete(data.id)}>Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Crud;
